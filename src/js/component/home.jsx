3a91ee2682166bb42d589a60e8810908b5c1199b
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-10-23 02:19:02 +0000

    Finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValor, setInputValor] = useState("");
+	const [toDo, setToDo] = useState([]);
+	const inputChange = (e) => {
+		setInputValor(e.target.value);
+	}
+	const enterKeyPress = (e) => {
+		if (e.key === "Enter"){
+			setToDo ([...toDo, inputValor]);
+			setInputValor ("");
+		}
+	}
+	const removeToDo = (index) => {
+		setToDo (toDo.filter ((_, i) => i !== index));
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To Do List</h1>
+			<div id="lista">
+				<ul className="list-group">
+					<li className="list-group-items">
+						<input
+							type="text"
+							value = {inputValor}
+							onChange={inputChange}
+							onKeyDown={enterKeyPress}
+							placeholder="What needs to be done?"></input>
+					</li>
+					<div className="container">
+					{toDo.map((item, index) => (
+						<li key={index}>
+							 <div key={index} className="list-item">
+      							<div className="list-item-content">
+        							{item}
+      							</div>
+      							<div className="list-item-button">
+        							<button id="boton" onClick={() => removeToDo(index)}>x</button>
+      							</div>
+    						</div>
+						</li>
+					))}</div>
+				</ul>
+			</div>
 		</div>
 	);
 };
